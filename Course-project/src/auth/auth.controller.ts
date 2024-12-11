eff27b23a056b26205d16ff34edd932f10bd5609
Oleksandr Poshtaruk <kievsash@ukr.net>
2024-12-11 19:58:07 +0200

    swagger params

--- Course-project/src/auth/auth.controller.ts
+++ Course-project/src/auth/auth.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Post, Body, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from './services/auth.service';
+import { ApiBody, ApiOperation } from '@nestjs/swagger';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
+  @ApiOperation({ summary: 'Login user' })
+  @ApiBody({ schema: { type: 'object', properties: { username: { type: 'string', description: 'The username of the user' }, password: { type: 'string', description: 'The password of the user' } }, required: ['username', 'password'] } })
   async login(@Body() body: { username: string; password: string }) {
     const user = await this.authService.validateUser(
       body.username,
@@ -20,6 +23,8 @@ export class AuthController {
   }
 
   @Post('refresh')
+  @ApiOperation({ summary: 'Refresh access token' })
+  @ApiBody({ schema: { type: 'object', properties: { refresh_token: { type: 'string', description: 'The refresh token used to obtain a new access token' } }, required: ['refresh_token'] } })
   async refresh(@Body() body: { refresh_token: string }) {
     const token = await this.authService.refreshAccessToken(body.refresh_token);
 
--- Course-project/src/posts/posts.controller.ts
+++ Course-project/src/posts/posts.controller.ts
@@ -12,7 +12,7 @@ import {
 } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { JwtAuthGuard } from '../auth/guards/oauth2.guard';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
 import { BodyParametersGuard } from '../guards/body-parameters-guard.service';
 
 @UseGuards(JwtAuthGuard)
@@ -30,6 +30,7 @@ export class PostsController {
   @Post()
   @UseGuards(new BodyParametersGuard(['username', 'content']))
   @ApiOperation({ summary: 'Create post for user' })
+  @ApiBody({ schema: { type: 'object', properties: { username: { type: 'string', description: 'The username of the user creating the post' }, content: { type: 'string', description: 'The content of the post' } }, required: ['username', 'content'] } })
   async createPost(@Body() body: { username: string; content: string }) {
     return this.postService.createPost(body.username, body.content);
   }
@@ -37,6 +38,7 @@ export class PostsController {
   @Put(':id')
   @UseGuards(new BodyParametersGuard(['content']))
   @ApiOperation({ summary: 'Update post for user by postId' })
+  @ApiParam({ name: 'id', type: 'number', description: 'The ID of the post to be updated' }) @ApiBody({ schema: { type: 'object', properties: { content: { type: 'string', description: 'The updated content of the post' } }, required: ['content'] } })
   async updatePost(
     @Param('id') id: number,
     @Body() body: { content: string },
@@ -46,6 +48,7 @@ export class PostsController {
 
   @Delete(':id')
   @ApiOperation({ summary: 'Delete post for user by postId' })
+  @ApiParam({ name: 'id', type: 'number', description: 'The ID of the post to be deleted' })
   async deletePost(@Param('id') id: number, @Request() req: any) {
     const user = req.user;
 
